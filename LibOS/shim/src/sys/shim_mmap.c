9a695825aa6e65997cbc0f14c3dda8016f20005e
Michał Kowalczyk <mkow@invisiblethingslab.com>
2019-12-04 22:56:09 +0100

    tmp

--- LibOS/shim/src/sys/shim_mmap.c
+++ LibOS/shim/src/sys/shim_mmap.c
@@ -39,7 +39,7 @@ void* shim_do_mmap(void* addr, size_t length, int prot, int flags, int fd, off_t
     long ret                = 0;
 
     if (length == 67108864 || length == 134217728) {
-        debug("XXXXX\n");
+        while(1);
     }
 
     /*
